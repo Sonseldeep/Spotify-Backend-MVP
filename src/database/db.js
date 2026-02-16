4a2ca1f8a3c90dec7264ef9146a55544b8a7c0af
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-02-16 22:55:52 +0545

    chore: initial project setup with db connection

---
+++ src/database/db.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+async function connectDb() {
+
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('Connected to MongoDB successfully');
+    }
+    catch(e) {
+        console.error('Error connecting to MongoDB:', e);
+    }
+}
+
+module.exports = connectDb;
