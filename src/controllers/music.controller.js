b7f09bf65733a7b071fee66c9bb46d11cb2d293c
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-02-17 22:06:08 +0545

    add role based controller for music create

---
+++ src/controllers/music.controller.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+const musicModel = require("../models/music.model");
+const jwt = require("jsonwebtoken");
+
+async function createMusic (req, res) {
+
+    const token = req.cookies.token;
+
+    if(!token) {
+        return res.status(401).json({
+            message : "Unauthorized"
+        });
+    }
+
+    try {
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        if (decoded.role !== "artist") {
+
+            return res.status(403).json({
+                message : "You don't have permission to create music"
+            });
+        }
+
+    } catch (error) {
+        res.status(401).json({
+            message : "Unauthorized"
+        });
+    }
+
+    const { title } = req.body;
+
+
+
+
+}
