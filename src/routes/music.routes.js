25aedd4b8ed54824616a990d6190b3041670f4cf
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-02-18 21:54:28 +0545

    chore: add route for music for user

--- src/routes/music.routes.js
+++ src/routes/music.routes.js
@@ -15,4 +15,6 @@ router.post("/upload",authMiddleware.authArtist, upload.single("music"), musicCo
 
 router.post("/album", authMiddleware.authArtist, musicController.createAlbum);
 
+router.get("/", musicController.getAllMusics);
+
 module.exports = router;
